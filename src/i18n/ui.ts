930644a0c4d6259568d60dcf84a0c56bbcf080a2
orlando-abm <obernalesm@gmail.com>
2024-03-07 10:18:29 -0300

    Add i18n for support multiples languages

---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const showDefaultLang = false;
+
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+
+export const defaultLang = 'es';
+
+export const ui = {
+    en: {
+      'nav.about': 'About',
+      'nav.experience': 'Experience',
+      'nav.education': 'Education',
+      'nav.skills': 'Skills',
+    },
+    es: {
+      'nav.about': 'Sobre mí',
+      'nav.experience': 'Experiencia Laboral',
+      'nav.education': 'Educación',
+      'nav.skills': 'Habilidades',
+    },
+  } as const;
